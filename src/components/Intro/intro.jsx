6fdea0ceb882354e5a6438aade60c1cc8ecf4597
Rav-Mihiranga <ravindumw99@gmail.com>
2025-11-26 04:44:17 +0530

    final_edit

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -3,11 +3,12 @@ import './intro.css';
 import bg from '../../assets/intro-bg.png';
 import hireMe from '../../assets/hireme.png';
 import Resume from '../../assets/resume.png';
+import AnchorLink from 'react-anchor-link-smooth-scroll'; 
 import {Link} from 'react-scroll';
 
 const Intro = () => {
   return (
-    <section id="intro">
+    <section id="home">
         <div className='introContent'>
             <span className="hello">Hello,</span>
             <span className="introText">I'm <span className="introName">Mihiranga</span> <br /> Software Developer </span>
@@ -15,13 +16,15 @@ const Intro = () => {
             <Link>
             <div className='introButtons'>
             <button className="btn">
-                <img src= {hireMe} alt="Hire Me" className='hireMe' />
+                <img src= {hireMe} alt="Hire Me" className='hireMe' /><AnchorLink className='anchor-link-hireme' offset={50} href='#Contact'>
                 Hire Me
+                </AnchorLink>
             </button>
+            <a href= "/public/resume.pdf" download="resume.pdf">
             <button className="cvbtn">
                 <img src= {Resume} alt="resume" className='resume' />
                 Resume
-            </button>
+            </button></a>
             </div>
             </Link>
         </div>
--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.jpg';
 import contactImg from '../../assets/contact-img.png';
 import theme_pattern from '../../assets/theme_pattern.svg';
+import underline from '../../assets/nav_underline.svg'
 import {Link} from 'react-scroll';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Navbar = () => {
 
+  const [menu,setMenu] = useState ("about")
+
   return (
     <nav className="navbar">
-      <img src={logo} alt='Logo' className='logo'/>
+      <AnchorLink className='anchor-link' offset={50} href='#home'>
+      <img src={logo} alt='Logo' className='logo'/></AnchorLink>
       <img src={theme_pattern} alt="" className='logo-img'/>
       <div className='desktopMenu'>
-        <Link className="desktopMenuListItem">Home</Link>
-        <Link className="desktopMenuListItem">About Me</Link>
-        <Link className="desktopMenuListItem">Services</Link>
-        <Link className="desktopMenuListItem">Portfolio</Link>
+        <li><AnchorLink className='anchor-link' href='#home'><p onClick={()=>setMenu("home")} className="desktopMenuListItem">Home </p></AnchorLink>{menu==="home"? <img src= {underline} alt=''/>:<></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")} className="desktopMenuListItem">About Me </p></AnchorLink>{menu==="about"? <img src= {underline} alt=''/>:<></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=>setMenu("services")} className="desktopMenuListItem">Services </p></AnchorLink>{menu==="services"? <img src= {underline} alt=''/>:<></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#MyWork'><p onClick={()=>setMenu("MyWork")} className="desktopMenuListItem">Portfolio </p></AnchorLink>{menu==="MyWork"? <img src= {underline} alt=''/>:<></>}</li>
 
       </div>
-      <button className='desktopMenuButton'>
+      <button className='desktopMenuButton'><AnchorLink className='anchor-link' offset={50} href='#Contact'>
         <img src= {contactImg} alt='' className='desktopMenuImg'/>
         Contact Me
+        </AnchorLink>
       </button>
 
     </nav>
